414f9a28890c156ad5277cb44d0e718ec5584d28
Jesper Kullberg <dev@jesperkullberg.com>
2021-02-14 11:15:15 +0100

    Started working on greeter window

--- src/applications/settings/Customization.tsx
+++ src/applications/settings/Customization.tsx
@@ -3,6 +3,7 @@ import darkPreview from "../../assets/img/preview-dark.svg";
 import lightPreview from "../../assets/img/preview-light.svg";
 import classicPreview from "../../assets/img/preview-classic.svg";
 import "../../assets/styles/componentStyles/Settings.css";
+import defaultBackground3 from "../../assets/img/backgrounds/edmond-lau-jba0W6Nssjg-unsplash.jpg";
 
 interface SettingsTypes {
   theme: string;
@@ -48,7 +49,7 @@ const Customization = ({ settings, setSettings }: CustomizationProps) => {
         JSON.stringify({
           ...newSettings,
           background:
-            "url(https://images.unsplash.com/photo-1497536003283-519132e25a7d)",
+            "url(" + defaultBackground3 + ")",
         })
       );
     }
